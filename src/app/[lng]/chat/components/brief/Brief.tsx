d7015b3429740e175d223c420bd0d0bb1f4a37a3
Vanessa <vanessatkd223@gmail.com>
2024-04-25 10:19:45 -0400

    changes: added type atom

--- src/app/[lng]/chat/components/brief/Brief.tsx
+++ src/app/[lng]/chat/components/brief/Brief.tsx
@@ -2,14 +2,19 @@
 import React, { useEffect, useState } from "react";
 import style from "./Brief.module.css";
 import BriefBody from "./BriefBody";
+import { useAtomValue } from "jotai";
+import { typeArea } from "@/atoms/type";
 
 export default function Brief() {
+
+  const type: any = useAtomValue(typeArea);
+
   return (
     <div className={style.main}>
       <div className={style.briefHeader}>
         <div className={style.briefHeaderTextContainer}>
           <p>
-            <span>Sales </span>Brief
+            <span>{ type } </span>Brief
           </p>
         </div>
       </div>
