7ee11b934e96ed6109f37b2a2d3913e3d3e46ab1
Vanessa Quero <vanessatkd223@gmail.com>
2024-04-28 22:15:25 -0400

    change: refactorized brief resume

--- src/app/[lng]/chat/components/brief/BriefResume.tsx
+++ src/app/[lng]/chat/components/brief/BriefResume.tsx
@@ -21,7 +21,12 @@ export default function BriefResume() {
     const { lng } = useParams();
     const { t } = useTranslation(lng, "resume");
     const type: any = useAtomValue(typeArea);
-    console.log(type,'type')
+
+    const getIndexLng = () => {
+        if ( type == Area.SALES ) return 'sales';
+        else return 'marketing'
+    }
+
     const resume : briefResume = {
         clientType: 'CEO',
         companySize: 40,
@@ -30,43 +35,21 @@ export default function BriefResume() {
         details: 'Empresa especializada en el desarrollo de soluciones de software personalizadas para empresas, con un enfoque particular en soluciones de automatización para la industria de manufactura y logística. Además, la empresa ofrece consultoría en transformación digital y servicios de mantenimiento de sistemas.'
     }
 
+
+    function getValueByKey( k : any) {
+        return resume[k];
+      }
+    
     return (
             <div className={style.card} >
-                { type == Area.SALES  ? 
-                    <>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}> {t(`BriefResume.sales.clientType.title`)} </p>
-                            <p className={style.resumeSectionDescription}>{resume.clientType}</p>
-                        </div>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}>{t(`BriefResume.sales.companySize.title`)}</p>
-                            <p className={style.resumeSectionDescription}>{resume.companySize}</p>
-                        </div>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}>{t(`BriefResume.sales.saleType.title`)}</p>
-                            <p className={style.resumeSectionDescription}>{resume.saleType}</p>
-                        </div>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}>{t(`BriefResume.sales.campaignDuration.title`)}</p>
-                            <p className={style.resumeSectionDescription}>{resume.campaignDuration}</p>
-                        </div>
-                        <div className={style.resumeSectionMore}>
-                            <p className={style.resumeSectionTitle}>{t(`BriefResume.sales.details.title`)}</p>
-                            <p className={style.resumeSectionDescription}>{resume.details}</p>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}> {t(`BriefResume.marketing.scenery.title`)} </p>
-                            <p className={style.resumeSectionDescription}>{resume.clientType}</p>
-                        </div>
-                        <div className={style.resumeSection}>
-                            <p className={style.resumeSectionTitle}>{t(`BriefResume.marketing.challenge.title`)}</p>
-                            <p className={style.resumeSectionDescription}>{resume.companySize}</p>
-                        </div>
-                    </>
-                }
+            {
+                Object.keys(resume).map((val, key) => (    
+                    <div className={val != 'details' ? style.resumeSection : style.resumeSectionMore}>
+                        <p className={style.resumeSectionTitle}> {t(`BriefResume.${getIndexLng()}.${val}.title`)} </p>
+                        <p className={style.resumeSectionDescription}>{`${getValueByKey(val)}`}</p>
+                    </div>    
+                ))
+            }
             </div>
     )
 }
